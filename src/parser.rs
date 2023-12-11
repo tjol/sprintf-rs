dc56dae5b0f76b0a63e40ea47b98d03346302b16
David Alexander Bjerremose <dab@davidbjerremose.com>
2023-12-11 14:31:42 +0100

    docs: properly updated doc comment

--- src/parser.rs
+++ src/parser.rs
@@ -77,21 +77,22 @@ pub enum ConversionType {
 ///     let fmt = "Hello %#06x";
 ///     let parsed = parse_format_string(fmt).unwrap();
 ///     assert_eq!(
-///        parsed,
-///        vec![
-///           FormatElement::Verbatim("Hello ".to_owned()),
-///           FormatElement::Format(ConversionSpecifier {
-///             alt_form: true,
-///             zero_pad: false,
-///             left_adj: false,
-///             space_sign: false,
-///             force_sign: false,
-///             width: NumericParam::Literal(6),
-///             precision: NumericParam::Literal(6),
-///             conversion_type: ConversionType::HexIntLower,
-///          }),
-///       ]
-///    );
+///         parsed,
+///         vec![
+///             FormatElement::Verbatim("Hello ".to_owned()),
+///             FormatElement::Format(ConversionSpecifier {
+///                 alt_form: true,
+///                 zero_pad: true,
+///                 left_adj: false,
+///                 space_sign: false,
+///                 force_sign: false,
+///                 width: NumericParam::Literal(6),
+///                 precision: NumericParam::Literal(6),
+///                 conversion_type: ConversionType::HexIntLower,
+///             }),
+///         ]
+///     );
+/// 
 ///
 pub fn parse_format_string(fmt: &str) -> Result<Vec<FormatElement>> {
     // find the first %
